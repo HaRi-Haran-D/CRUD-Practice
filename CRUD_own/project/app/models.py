d8ddb8aa175122002d0965ab6dff32141bff7eed
HaRi-Haran-D <hvj58128@gmail.com>
2026-02-01 16:08:47 +0530

    Practice

--- CRUD_own/project/app/models.py
+++ CRUD_own/project/app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Company(models.Model):
     emp_id = models.IntegerField(unique=True, blank=True)
     emp_name = models.CharField(max_length=50)
-    occupation = models.CharField(maxlength=50)
+    occupation = models.CharField(max_length=50)
     salary = models.IntegerField()
     joining_date = models.DateField(auto_now_add=True)
 
