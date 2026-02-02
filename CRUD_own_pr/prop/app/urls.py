2fc777abf67bc3c6aa8eb08f8e9c63f53e97f99a
HaRi-Haran-D <hvj58128@gmail.com>
2026-02-02 20:12:33 +0530

    Practice

--- CRUD_own_pr/prop/app/urls.py
+++ CRUD_own_pr/prop/app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('list/',views.list, name='list'),
+    path('update/<int:item_id>/', views.update, name='update'),
+    path('delete/<int:item_id/', views.delete, name='delete')
 ]
--- CRUD_own_pr/prop/app/views.py
+++ CRUD_own_pr/prop/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from .forms import StudentForm
 from .models import Student
 
@@ -18,3 +19,21 @@ def home(request):
 def list(request):
     list = Student.objects.all()
     return render(request, 'list.html', {'list':list})
+
+
+def update(request,item_id):
+    item = Student.objects.get(id=item_id)
+    form = StudentForm(instance=item)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    return render(request, 'update.html', {'form':form})
+
+def delete(request, item_id):
+    std = get_object_or_404(Student, id=item_id)
+    if request.method == 'POST':
+        std.delete()
+        return redirect('list')
+    return redirect('list')
