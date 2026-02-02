a090d7186a077551b6005e20ba8dd5012898c67f
HaRi-Haran-D <hvj58128@gmail.com>
2026-02-02 20:17:38 +0530

    Practice

--- CRUD_own_pr/prop/app/urls.py
+++ CRUD_own_pr/prop/app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('list/',views.list, name='list'),
     path('update/<int:item_id>/', views.update, name='update'),
-    path('delete/<int:item_id/', views.delete, name='delete')
+    path('delete/<int:item_id>/', views.delete, name='delete')
 ]
