8e81662c7b4a3de3b3053e50ee87aba81de35785
HaRi-Haran-D <hvj58128@gmail.com>
2026-02-03 21:55:26 +0530

    Practice

---
+++ Home_project/prop/app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .models import User
+from .forms import UserForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = UserForm()
+            return redirect('home')
+    else:
+        form = UserForm()
+    return render(request, 'register.html', {'form':form})
+
+def login(request, username):
+    pass
