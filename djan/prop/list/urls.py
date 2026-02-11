0e4d691991c670e88889c56c94cc1d8df960b40a
HaRi-Haran-D <hvj58128@gmail.com>
2026-02-11 10:33:32 +0530

    Practice

--- djan/prop/list/urls.py
+++ djan/prop/list/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+app_name = 'list'
+
 urlpatterns = [
-    path('', views.list, name='list'),
+    path('', views.post, name='post'),
+    path('<slug:slug>', views.post_list, name='post_list')
 ]
--- djan/prop/list/views.py
+++ djan/prop/list/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import List
 
 # Create your views here.
-def list(request):
-    return render(request, 'list/list.html')
+def post(request):
+    lists = List.objects.all().order_by('-date')
+    return render(request, 'list/list.html', {'lists':lists})
+
+def post_list(request, slug):
+    post = List.objects.get(slug=slug)
+    return render(request, 'list/post_list.html', {'post':post})
