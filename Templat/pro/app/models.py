bbeeb1ea856c8406534dab213a55c48445fc8bb4
HaRi-Haran-D <hvj58128@gmail.com>
2026-03-01 22:31:11 +0530

    Practice

--- Templat/pro/app/models.py
+++ Templat/pro/app/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
     descri = models.CharField(max_length=100)
+    image = models.CharField(max_length=500,default='https://www.foodservicerewards.com/cdn/shop/t/262/assets/fsr-placeholder.png?v=45093109498714503231652397781')
 
     def __str__(self):
         return self.name
