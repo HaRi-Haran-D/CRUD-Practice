8d1829c68fd78cb2e7520fcf48ad975a8a808df8
HaRi-Haran-D <hvj58128@gmail.com>
2026-03-01 12:24:48 +0530

    Practicce

--- Templat/pro/app/views.py
+++ Templat/pro/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+def home(request):
+    items = Item.objects.all()
+    return render(request, 'app/base.html', {'items':items})
+
