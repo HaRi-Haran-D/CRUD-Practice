32ed5c6d0affdafdffe6bcf2b48388770ba250b4
HaRi-Haran-D <hvj58128@gmail.com>
2026-03-17 22:36:55 +0530

    Practice

--- Templat/pro/users/views.py
+++ Templat/pro/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib import messages
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
 def register(request):
@@ -18,3 +20,7 @@ def register(request):
 def logout_view(request):
     logout(request)
     return render(request, 'users/logout.html')
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
