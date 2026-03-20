5c501dede02af30e1fb212e1186d9a39d858a55b
HaRi-Haran-D <hvj58128@gmail.com>
2026-03-20 12:00:28 +0530

    Practice

--- crud_dem/pro/app/urls.py
+++ crud_dem/pro/app/urls.py
@@ -4,4 +4,7 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.add, name='add'),
+    path('lists/', views.listdata, name='lists'),
+    path('update/<int:id>/', views.updatedata, name='update'),
+    path('delete/<int:id>/', views.deletedata, name='delete')
 ]
--- crud_dem/pro/app/views.py
+++ crud_dem/pro/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import StudentForm
 from .models import Student
@@ -11,3 +11,26 @@ def add(request):
         messages.success(request, "Form has be added")
         return redirect('app:add')
     return render(request, 'app/add.html', {'form':form})
+
+
+def listdata(request):
+    lists = Student.objects.all()
+    return render(request, 'app/list.html', {'lists':lists})
+
+
+def updatedata(request, id):
+    value = Student.objects.get(id=id)
+    form = StudentForm(request.POST or None, instance=value)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Form has been Updated Successfully")
+        return redirect('app:lists')
+    return render(request, 'app/add.html', {'form':form})
+
+
+def deletedata(request, id):
+    data = get_object_or_404(Student, id=id)
+    if request.method == 'POST':
+        data.delete()
+        return redirect('app:lists')
+    return redirect('app:lists')
