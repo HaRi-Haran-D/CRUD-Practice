bc6bb205493f13e3af76031c5fe8da0cfc37f2b4
HaRi-Haran-D <hvj58128@gmail.com>
2026-03-21 12:14:12 +0530

    Practice

--- crud_with_View/pro/app/urls.py
+++ crud_with_View/pro/app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+app_name = 'app'
 
 urlpatterns = [
-    path('', ),
+    path('', views.home, name='home'),
+    path('create/', views.CreateStudent.as_view(), name='create'),
+    path('list/', views.StudentList.as_view(), name='list'),
+    path('detail/<int:pk>/', views.StudentDetail.as_view(), name='detail'),
+    path('update/<int:pk>/', views.UpdateStudent.as_view(), name='update'),
+    path('delete/<int:pk>/', views.DeleteStudent.as_view(), name='delete'),
 ]
--- crud_with_View/pro/app/views.py
+++ crud_with_View/pro/app/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.urls import reverse_lazy
 from .models import Student
 
 # Create your views here.
-class StudentList(CreateView):
+def home(request):
+    return render(request, 'app/home.html')
+
+class CreateStudent(CreateView):
     model = Student
-    template_name = 'form.html'
-    
+    fields = '__all__'
+    template_name = 'app/form.html'
+
+
+class StudentList(ListView):
+    model = Student
+    template_name = 'app/student_list.html'
+    context_object_name = 'lists'
+
+
+class StudentDetail(DetailView):
+    model = Student
+    template_name = 'app/detail.html'
+    context_object_name = 'list'
+
+class UpdateStudent(UpdateView):
+    model = Student
+    fields = '__all__'
+    template_name = 'app/form.html'
+
+class DeleteStudent(DeleteView):
+    model = Student
+    success_url = reverse_lazy('app:home')
