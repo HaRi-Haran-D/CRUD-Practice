c5ca8bf1c7e51fbeccf90fac26fbbd0b2dcadeb8
HaRi-Haran-D <hvj58128@gmail.com>
2026-03-28 22:47:52 +0530

    Practice

--- Templat/pro/app/models.py
+++ Templat/pro/app/models.py
@@ -6,12 +6,21 @@ from django.urls import reverse
 class Item(models.Model):
     user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=50)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
     descri = models.CharField(max_length=100)
-    image = models.CharField(max_length=500,default='https://www.foodservicerewards.com/cdn/shop/t/262/assets/fsr-placeholder.png?v=45093109498714503231652397781')
+    image = models.URLField(max_length=500,default='https://www.foodservicerewards.com/cdn/shop/t/262/assets/fsr-placeholder.png?v=45093109498714503231652397781')
+    is_availablle = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('app:home')
+ 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    added_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
