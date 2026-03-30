f5244c9db1505fe568b966179421dcb32e339a17
HaRi-Haran-D <hvj58128@gmail.com>
2026-03-30 22:43:29 +0530

    Practice

--- Templat/pro/app/models.py
+++ Templat/pro/app/models.py
@@ -4,20 +4,24 @@ from django.urls import reverse
 
 # Create your models here.
 class Item(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=['name','price'])
+        ]
     user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    name = models.CharField(max_length=50, db_index=True)
+    price = models.DecimalField(max_digits=6,decimal_places=2, db_index=True)
     descri = models.CharField(max_length=100)
     image = models.URLField(max_length=500,default='https://www.foodservicerewards.com/cdn/shop/t/262/assets/fsr-placeholder.png?v=45093109498714503231652397781')
     is_availablle = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.name + ": " + str(self.price)
     
     def get_absolute_url(self):
         return reverse('app:home')
- 
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     added_on = models.DateTimeField(auto_now=True)
