b86423fc4b880f33873d6e403e84f95dbd04debf
HaRi-Haran-D <hvj58128@gmail.com>
2026-08-12 10:11:25 +0530

    Django API Practice

---
+++ New/Users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    ph_no = models.CharField(max_length=10, unique=True)
