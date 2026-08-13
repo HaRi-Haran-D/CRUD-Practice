fa56d1bc3ceb7c175ffd87cf5b6b8d43b234ec75
HaRi-Haran-D <hvj58128@gmail.com>
2026-08-13 12:21:33 +0530

    DRF practice

--- New/Users/views.py
+++ New/Users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from .serializers import *
+from .models import *
+
 
 # Create your views here.
+
+class UserView(generics.CreateAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
